93056afee525aed9133066812d9f41ac6da279ca
Vincent Ferrard <vincent.ferrard@epitech.eu>
2021-04-19 19:26:41 +0200

    [EDIT] connect register back to front

--- server/src/server.js
+++ server/src/server.js
@@ -11,6 +11,13 @@ const options = {
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 mongoose.connect(MONGOURL, options)
 .then(() => console.log("DB connected"))
 .catch(error => console.log(error));
@@ -50,7 +57,7 @@ app.post ('api/user/signin', (req, res) => {
     })
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`server running on ${port}`);
